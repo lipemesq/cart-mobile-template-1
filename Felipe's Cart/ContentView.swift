cb021cf0afd95004157b7c7676899e609c36da77
Felipe Mesquita <lipemesq@gmail.com>
2020-11-20 13:13:33 -0300

    Initial Commit

---
+++ Felipe's Cart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Felipe's Cart
+//
+//  Created by Felipe Mesquita on 20/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
